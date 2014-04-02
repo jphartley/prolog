1b37eb16bfc26d93f2558fdd9f8ebc1e93f38895
jphartley <jphartley@gmail.com>
2014-04-02 21:01:29 +0200

    Inital Commit of 5 prolog files

---
+++ books.pl
@@ -0,0 +1,4 @@
+book(huxley, brave_new_world).
+book(huxley, island).
+book(hamilton, judas-unchained).
+book(orwell, 1984).
---
+++ food.pl
@@ -0,0 +1,11 @@
+food_type(velveeta, cheese).
+food_type(ritz, cracker).
+food_type(spam, meat).
+food_type(sausage, meat).
+food_type(twinkie, dessert).
+food_type(jolt, soda).
+flavor(sweet, dessert).
+flavor(savory, meat).
+flavor(savory, cheese).
+flavor(sweet, soda).
+food_flavor(X,Y) :- food_type(X,Z), flavor(Y,Z).
